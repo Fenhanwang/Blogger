0582f053aaafe4c607fbf2fa2ef64fdd641acb52
Fenhan <wfenhan@gmail.com>
2015-05-01 12:05:43 -0400

    finishied blog comments feature

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,8 @@ ActiveRecord::Schema.define(version: 20150428004842) do
   create_table "articles", force: true do |t|
     t.string   "title"
     t.text     "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "comments", force: true do |t|
