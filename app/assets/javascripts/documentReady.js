a414beac7b955058445d3e8ffc7e27136e53a606
Fenhan <wfenhan@gmail.com>
2015-07-23 15:57:18 -0400

    Complete iframe function

--- app/assets/javascripts/documentReady.js
+++ app/assets/javascripts/documentReady.js
@@ -6,5 +6,10 @@ $(document).ready(function(){
   }, 2000);
 
 
+  // javascript in Pages Controller
+  $('#Fenhaniframe-pages').width($('#Fenhanjumbotron-Pages').width());
+    $( window ).resize(function() {
+    	$('#Fenhaniframe-pages').width($('#Fenhanjumbotron-Pages').width());
+    })
 
  })
